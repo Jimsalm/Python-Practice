b14203300797763b681d6996df8f78dadf649a36
Jimsalm <hypermood15.jimiel@gmail.com>
2025-08-19 22:58:58 +0800

    consonant vowel counter

---
+++ vowerconscounter.py
@@ -0,0 +1,16 @@
+
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+vowel_count = 0
+consonant_count = 0
+
+user_input = input("\nEnter a word or sentence: ")
+
+for letter in user_input:
+    if letter in vowels:
+        vowel_count += 1
+    elif letter.isalpha():
+        consonant_count += 1
+
+print(f'Vowels: {vowel_count}')
+print(f'Consonant: {consonant_count}')
